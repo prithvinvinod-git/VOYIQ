d881c3b340fcfe5f8d9d9217311fc9b3d21b3e51
Prithvin Vinod <prithvinvinod575@gmail.com>
2026-03-16 11:22:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7641429605-be4cb",
+  "appId": "1:492257467505:web:89eea7f7817df432004fd0",
+  "apiKey": "AIzaSyBnK3klbTQfFxbWh4sb_QJ688_jF1XZR9I",
+  "authDomain": "studio-7641429605-be4cb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "492257467505"
+};
