3c968dcf95338d8778786d2524a5178c485888a9
Prithvin Vinod <prithvinvinod575@gmail.com>
2026-03-16 12:23:37 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -22,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=Syne:wght@600;700;800&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin="" />
       </head>
-      <body className="font-body antialiased bg-background text-foreground min-h-screen">
+      <body className="font-body antialiased bg-background text-foreground min-h-screen" suppressHydrationWarning>
         <FirebaseClientProvider>
           {children}
         </FirebaseClientProvider>
